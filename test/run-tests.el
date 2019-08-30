c0eddf8d73c85471944a77ea59be8da78fbc7372
marcihm <marc.ihm@schufa.de>
2019-08-30 06:32:21 +0200

    adapting inherited tests

---
+++ test/run-tests.el
@@ -0,0 +1,32 @@
+(setq load-prefer-newer t)
+(add-to-list 'load-path "c:/org-mode/lisp")
+(add-to-list 'load-path "c:/org/custom")
+(add-to-list 'load-path "c:/org-index")
+(add-to-list 'load-path "c:/org-index/test")
+
+(setq package-user-dir "c:/org/custom/elpa")
+(package-initialize)
+
+(require 'org)
+(require 'org-index)
+(require 'oidxt)
+(require 'paredit)
+(autoload 'paredit-mode "paredit" "Minor mode for pseudo-structurally editing Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode +1)))
+
+(setq oidxt-keep-test-state t)
+
+(setq org-id-locations-file "~\\.emacs.d\\.org-id-locations-for-test")
+(setq custom-theme-directory "c:/org/custom/themes")
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode nil)
+(scroll-bar-mode -1)
+(setq inhibit-splash-screen t)
+(set-variable 'make-backup-files nil)
+(setq ring-bell-function 'ignore)
+(global-font-lock-mode 1)
+(toggle-truncate-lines)
+(setq initial-frame-alist '((width . 122) (height . 42) (top . 20) (left . 50) (name . "emacs")))
+
---
+++ test/run-tests.ps1
@@ -0,0 +1,2 @@
+
+C:\emacs\emacs-26.2\bin\runemacs.exe --debug-init -fn "-outline-Lucida Console-normal-r-normal-normal-18-97-96-96-c-*-iso8859-1" -l C:/org-working-set/test/run-tests.el C:/org-working-set/org-working-set.el C:/org-working-set/test/org-wst.el C:/org-working-set/test/run-tests.el C:/org-working-set/test/run-tests.ps1
