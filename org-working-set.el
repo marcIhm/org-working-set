c8dda4b0ea4e8fe1a87544a75f257123609c5d27
Marc Ihm <marc.ihm@schufa.de>
2019-09-20 14:19:00 +0200

    move by logical lines in working-set buffer

--- org-working-set.el
+++ org-working-set.el
@@ -527,6 +527,8 @@ Argument KEY has been pressed to trigger this function."
 Optional argument RESIZE adjusts window size."
   (let (cursor-here lb)
     (with-current-buffer (get-buffer-create org-working-set--menu-buffer-name)
+      (make-local-variable line-move-visual)
+      (setq line-move-visual nil)
       (setq buffer-read-only nil)
       (erase-buffer)
       (insert (propertize (if org-working-set--short-help-wanted
